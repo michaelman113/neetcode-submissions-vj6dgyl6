15d578041e6f19dff1c8552c8abf5b6fdff8ded5
michaelman113 <159601545+michaelman113@users.noreply.github.com>
2026-06-23 08:47:15 -0400

    Add: reverse-string - submission-1

---
+++ Data Structures & Algorithms/reverse-string/submission-1.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            int temp = s[l];
+            s[l] = s[r];
+            s[r] = temp;
+            l++;
+            r--;
+        }
+        
+    }
+};
